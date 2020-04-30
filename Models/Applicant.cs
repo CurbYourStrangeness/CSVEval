fc5c39bd60fa2b898e37b4584cff6f13d7237bff
J Miller <devbuild2018@gmail.com>
2020-04-30 10:45:48 -0500

    Initial commit

---
+++ Models/Applicant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVEval.Models
+{
+    class Applicant
+    {
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime DOB { get; set; }
+
+    public string PlanType { get; set; }
+
+    public DateTime EffectiveDate { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSVEval
+ HSA Bank Sample Project
