589d50aa039774797c1dccdab1fe20b8de528759
J Miller <devbuild2018@gmail.com>
2020-05-02 10:19:38 -0500

    Uncommented some debug lines

--- Models/ReadApplicantModel.cs
+++ Models/ReadApplicantModel.cs
@@ -11,14 +11,14 @@ namespace CSVEval.Models
     {
         [Index(0)]
         public string FirstName { get; set; }
-        //[Index(1)]
-        //public string LastName { get; set; }
-        //[Index(2)]
-        //public string DOB { get; set; }
-        //[Index(3)]
-        //public string PlanType { get; set; }
-        //[Index(4)]
-        //public string EffectiveDate { get; set; }
+        [Index(1)]
+        public string LastName { get; set; }
+        [Index(2)]
+        public string DOB { get; set; }
+        [Index(3)]
+        public string PlanType { get; set; }
+        [Index(4)]
+        public string EffectiveDate { get; set; }
 
     }
 
